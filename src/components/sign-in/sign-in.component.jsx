827ca471bc8d8b643923224f2362555a63ca11ad
leah-h <harrichl@gmail.com>
2020-12-09 04:49:42 -0500

    add custom button with styling

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 
 import "./sign-in.styles.scss"
 
@@ -35,7 +36,7 @@ class SignIn extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                 <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label = "Email" required/>
                 <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="Password" required/>
-                <input type="submit" value="Submit" />
+                <CustomButton type="submit">SIGN IN</CustomButton>
                 </form>            
             </div>
         )
