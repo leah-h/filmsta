286fb32625d5e2afa42d79fca396144dbd45f6ea
leah-h <harrichl@gmail.com>
2020-12-10 04:25:47 -0500

    store user data in firestore app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,33 @@ const config = {
     appId: "1:561709988621:web:44d2e5fbf670f3da7d0e44",
     measurementId: "G-HM0MWZ8H7Q"
   };
+  
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName, 
+        email, 
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+        console.log('error creating user ', error.message)
+    }
+  }
+
+  return userRef;
+}  
 
 firebase.initializeApp(config);
 
